0f2c0f7ba5cc26ba164e3d5a9f4b5410e1ef895f
Matías Cristaldo <matias.cristaldo@utec.edu.uy>
2021-04-27 19:18:07 -0300

    Registro

--- tipymovies/resources/views/home.blade.php
+++ tipymovies/resources/views/home.blade.php
@@ -1,59 +1,12 @@
 <!DOCTYPE html>
-<!-- @extends('layout') -->
+@extends('layout')
 
-<!-- @section('content') -->
-
+@section('content')
 <html lang="">
 <head>
 <title>Tipy Movies</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="../resources/css/layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
-<div class="bgded overlay" style="background-image:url('C:/xampp/htdocs/Php/TIpymovies/tipymovies/public/images/demo/backgrounds/03.png');"> 
-
-  <div class="wrapper row0">
-    <div id="topbar" class="hoc clear"> 
-    
-      <div class="fl_left">
-        <ul class="nospace">
-          <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
-          <li><a href="#">Acerca de</a></li>
-          <li><a href="#">Contacto</a></li>
-          <li><a href="#">Iniciar Sesion</a></li>
-          <li><a href="#">Registrarse</a></li>
-        </ul>
-      </div>
-      <div class="fl_right">
-        <ul class="nospace">
-          <li><i class="fa fa-envelope-o"></i> por_si_ponemos@correo_de_contacto.com</li>
-        </ul>
-      </div>
-      
-    </div>
-  </div>
-  
-  <div class="wrapper row1">
-    <header id="header" class="hoc clear"> 
-    
-      <div id="logo" class="fl_left">
-        <h1><a href="index.html">Tipy movies</a></h1>
-      </div>
-      
-      <nav id="mainav" class="fl_right">
-        <ul class="clear">
-          <li class="active"><a href="index.html">Inicio</a></li>
-          <li><a href="index.html">Peliculas</a></li>
-          <li><a href="index.html">Series</a></li>
-          <li><a href="index.html">Trivia</a></li>
-          <li><input type="search" name="buscar"></a></li>
-        </ul>
-      </nav>
-      
-    </header>
-  </div>
-
   <div class="wrapper">
     <div id="pageintro" class="hoc clear"> 
       <article>
@@ -70,7 +23,6 @@
       </article>
     </div>
   </div>
-  <div class="one_half"><img class="inspace-10 borderedbox" src="C:/xampp/htdocs/Php/TIpymovies/tipymovies/public/images/demo/imgl.png" alt=""></div>
 </div>
 <!-- JAVASCRIPTS -->
 <script src="../resources/js/scripts/jquery.min.js"></script>
@@ -78,4 +30,5 @@
 <script src="../resources/js/scripts/jquery.mobilemenu.js"></script>
 </body>
 </html>
-<!-- @endsection -->
+
+@endsection('content')
