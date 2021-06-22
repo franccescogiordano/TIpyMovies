1cb674533bb79e736baffd73366a932d99e5bf5f
Santiago Rosales <68907312+santirosales@users.noreply.github.com>
2021-06-22 18:48:17 -0300

    porque da error

--- tipymovies/resources/views/AboutAS.blade.php
+++ tipymovies/resources/views/AboutAS.blade.php
@@ -9,24 +9,27 @@
     <h2>Acerca de TIPYMOVIES:</h2>
     <p>Buscamos ser referentes de la pirateria de peliculas, ofreciendo una basta cantidad de paginas web de dudosa procedencia con pelicula taquilleras del ayer y hoy.</p>
 </div>
-<div class="container">
-    <form action="action_page.php">
+<div>
+    <h2>Contacta con nosotros:</h2>
+    <div class="container">
+        <form action="action_page.php">
 
-        <label for="fname">Nombre</label>
-        <input type="text" id="fname" name="firstname" placeholder="Nombre">
+            <label for="fname">Nombre</label>
+            <input type="text" id="fname" name="firstname" placeholder="Nombre">
 
-        <label for="lname">Apellido</label>
-        <input type="text" id="lname" name="lastname" placeholder="Apellido">
+            <label for="lname">Apellido</label>
+            <input type="text" id="lname" name="lastname" placeholder="Apellido">
 
-        <label for="country">País</label>
-        <input type="text" id="Cname" name="Country" placeholder="País">
+            <label for="country">País</label>
+            <input type="text" id="Cname" name="Country" placeholder="País">
 
-        <label for="subject">Mensaje</label>
-        <textarea id="subject" name="subject" placeholder="Escriba su mensaje" style="height:200px"></textarea>
+            <label for="subject">Mensaje</label>
+            <textarea id="subject" name="subject" placeholder="Escriba su mensaje" style="height:200px"></textarea>
 
-        <input type="submit" value="Submit">
+            <input type="submit" value="Submit">
 
-    </form>
+        </form>
+    </div>
 </div>
 
 
