16659f526d5bc2891c3f2ead0903f77a8f7abfc9
Santiago Rosales <68907312+santirosales@users.noreply.github.com>
2021-07-06 19:18:46 -0300

    nuevo

--- tipymovies/resources/views/layouts/app.blade.php
+++ tipymovies/resources/views/layouts/app.blade.php
@@ -24,8 +24,6 @@
         <div id="topbar" class="hoc clear">
         <div class="fl_left">
             <ul class="nospace">
-           <li><a href="{{ route('home') }}"> <img src="{{asset('images/house.png')}}"><i></i></a></li>
-            <li><a href="#">Acerca de</a></li>
             <li><a href="#">Contacto</a></li>
             </ul>
 
@@ -64,7 +62,7 @@
 
 
 				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-					<a class="nav-link" href="{{ route('home')}}">TIPY MOVIES</a>
+				<a class="nav-link" href="{{ route('home')}}"><img class="logotipymovies" src="{{asset('images/alras.png')}}"></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
--- tipymovies/routes/web.php
+++ tipymovies/routes/web.php
@@ -92,9 +92,6 @@ Route::POST('/MiniJuego1/Puntuacion/{imdbID}/{titulo}',[PreguntasController::cla
 Route::POST('/MiniJuego1/Puntuacion',[PreguntasController::class,'puntuar2'])->name('Puntuar2');
 
 Route::GET('/Ranking', [PreguntasController::class,'getTopScore'])->name('ranking');
-//Route::POST('/',[PreguntasController::class,'puntuar'])->name('puntuar');
 Route::view('/contactUs', 'contactUs')->name('ContactUs');
-//Route::GET('contact-us', 'ContactUsController@contactUs');
 Route::GET('/contact-us', [ContactUsController::class,'contactUs'])->name('contact-us');
-//Route::POST('contact-us',['as'=>'contactus.store','uses'=>'ContactUsController@contactUsPost']);
 Route::POST('/contactUs', [ContactUsController::class,'contactUsPost'])->name('contactus.store');
